5e04ae7604c98f0b53c53c227304211ba104a70c
Jeff Swisher <jefftswisher@gmail.com>
2020-01-20 23:04:47 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
+  has_many :characters 
+  has_many :actors, through: :characters
   
+  def actors_list 
+    actors.collect do |f|
+      "#{f.first_name} #{f.last_name}" 
+    end 
+  end 
 end
